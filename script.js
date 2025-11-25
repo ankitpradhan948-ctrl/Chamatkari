b3ab0397f36842603bdfe20f7eea53c102d04265
ankitpradhan948-ctrl <ankitpradhan948@gmail.com>
2025-11-26 02:08:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openPDF(pdfName) {
+    window.open(pdfName, "_blank");
+}
+function showMessage() {
+    let msg = document.getElementById("message");
+    msg.textContent = "ummm hmmm bade log !";
+    msg.style.opacity = 1;
+}
+
+
